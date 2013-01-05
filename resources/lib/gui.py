b1289849c0c6c8359a50f9366769e8606de6adb5
poedelz <poedelz@db7c0809-cc3b-abc7-1a57-eb63581bcc4c>
2013-01-05 03:09:17 +0000

    update
    git-svn-id: http://ronie.googlecode.com/svn/trunk/screensaver.xbmc.slideshow@527 db7c0809-cc3b-abc7-1a57-eb63581bcc4c

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -13,13 +13,13 @@
 # *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 # *  http://www.gnu.org/copyleft/gpl.html
 
-# TODO conditional exif rotation
-# TODO conditional zoom based on slideshow time
+# TODO conditional rotate based on exif info
 
 import os, sys, random, urllib
 import xbmc, xbmcgui, xbmcaddon, xbmcvfs
 from xml.dom.minidom import parse
 from PIL import Image
+from PIL.ExifTags import TAGS
 if sys.version_info < (2, 7):
     import simplejson
 else:
@@ -34,15 +34,19 @@ __skinxbmc__ = xbmc.translatePath( os.path.join( 'special://xbmc/addons/', __ski
 
 IMAGE_TYPES = ('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.ico', '.tif', '.tiff', '.tga', '.pcx')
 
-EFFECTLIST = ["('effect=slide start=0,0 end=0,0 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=400 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=1280,0 end=-1280,0 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=-1280,0 end=1280,0 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=0,720 end=0,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=0,-720 end=0,720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=1280,720 end=-1280,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=-1280,720 end=1280,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=1280,-720 end=-1280,720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')",
-             "('effect=slide start=-1280,-720 end=1280,720 time=%i condition=true', 'conditional'), ('effect=zoom start=100 end=100 center=auto time=%i condition=true', 'conditional')"]
+EXIF_TYPES = ('.jpg', '.jpeg', '.tif', '.tiff')
+
+ANGLE = {'1': '0', '2': '0', '3': '180', '4': '0', '5': '0', '6': '90', '7': '0', '8': '270'}
+
+EFFECTLIST = ["('effect=zoom start=100 end=400 center=auto time=%i condition=true', 'conditional'),",
+             "('effect=slide start=1280,0 end=-1280,0 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=-1280,0 end=1280,0 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=0,720 end=0,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=0,-720 end=0,720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=1280,720 end=-1280,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=-1280,720 end=1280,-720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=1280,-720 end=-1280,720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')",
+             "('effect=slide start=-1280,-720 end=1280,720 time=%i condition=true', 'conditional'), ('effect=zoom start=%i end=%i center=auto time=%i condition=true', 'conditional')"]
 
 def log(txt):
     if isinstance (txt,str):
@@ -56,7 +60,8 @@ class Screensaver(xbmcgui.WindowXMLDialog):
 
     def onInit(self):
         # load constants
-        self._get_conts()
+        self._get_vars()
+        # get addon settings
         self._get_settings()
         # get the effectslowdown value from the current skin
         effectslowdown = self._get_animspeed()
@@ -74,7 +79,7 @@ class Screensaver(xbmcgui.WindowXMLDialog):
             # start slideshow
             self._start_show(items)
 
-    def _get_conts(self):
+    def _get_vars(self):
         self.winid   = xbmcgui.Window(xbmcgui.getCurrentWindowDialogId())
         self.Monitor = MyMonitor(action = self._exit)
         self.stop    = False
@@ -85,14 +90,13 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self.slideshow_path   = __addon__.getSetting('path')
         self.slideshow_effect = __addon__.getSetting('effect')
         self.slideshow_time   = int(__addon__.getSetting('time'))
-        self.slideshow_dim    = hex(int('%.0f' % (float(__addon__.getSetting('level')) * 2.55)))[2:] + 'ffffff' # convert float to hex value usable by the skin
+        # convert float to hex value usable by the skin
+        self.slideshow_dim    = hex(int('%.0f' % (float(__addon__.getSetting('level')) * 2.55)))[2:] + 'ffffff'
         self.slideshow_scale  = __addon__.getSetting('scale')
         # select which image controls from the xml we are going to use
         if self.slideshow_scale == 'false':
             self.image1 = self.getControl(1)
             self.image2 = self.getControl(2)
-            self.getControl(1).setVisible(False)
-            self.getControl(2).setVisible(False)
             self.getControl(3).setVisible(False)
             self.getControl(4).setVisible(False)
         else:
@@ -100,47 +104,69 @@ class Screensaver(xbmcgui.WindowXMLDialog):
             self.image2 = self.getControl(4)
             self.getControl(1).setVisible(False)
             self.getControl(2).setVisible(False)
-            self.getControl(3).setVisible(False)
-            self.getControl(4).setVisible(False)
         self.slideshow_name = __addon__.getSetting('label')
         if self.slideshow_name == '0':
             self.getControl(99).setVisible(False)
         else:
             self.namelabel = self.getControl(99)
-        # add 1 sec fadeout time to showtime
-        self.anim_time = self.slideshow_time + 1
         # set the dim property
         self._set_prop('Dim', self.slideshow_dim)
 
-    def _set_prop(self, name, value):
-        self.winid.setProperty('SlideView.%s' % name, value)
-
-    def _clear_prop(self, name):
-        self.winid.clearProperty('SlideView.%s' % name)
-
-    def _get_animspeed(self):
-        # find the skindir
-        if xbmcvfs.exists( __skinxbmc__ ):
-            # xbmc addon dir
-            skinxml = __skinxbmc__
-        elif xbmcvfs.exists( __skinhome__ ):
-            # user addon dir
-            skinxml = __skinhome__
-        else:
-            return
-        try:
-            # parse the skin addon.xml
-            self.xml = parse(skinxml)
-            # find all extension tags
-            tags = self.xml.documentElement.getElementsByTagName( 'extension' )
-            for tag in tags:
-                # find the effectslowdown attribute
-                for (name, value) in tag.attributes.items():
-                    if name == 'effectslowdown':
-                        anim = value
-                        return anim
-        except:
-            return
+    def _start_show(self, items):
+        # start with image 1
+        cur_img = self.image1
+        order = [1,2]
+        # loop until onScreensaverDeactivated is called
+        while (not xbmc.abortRequested) and (not self.stop):
+            # iterate through all the images
+            for img in items:
+                # add image to gui
+                cur_img.setImage(img)
+                # give xbmc some time to load the image
+                if not self.startup:
+                    xbmc.sleep(1000)
+                else:
+                    self.startup = False
+                # get the file or foldername if enabled in settings
+                if self.slideshow_name != '0':
+                    if self.slideshow_name == '1':
+                        NAME, EXT = os.path.splitext(os.path.basename(img))
+                    elif self.slideshow_name == '2':
+                        ROOT, NAME = os.path.split(os.path.dirname(img))
+                    self.namelabel.setLabel(NAME)
+#                # check if we need to rotate the image (ONLY WORKS FOR LOCAL FILES)
+#                orientation = 0
+#                if os.path.splitext(img)[1].lower() in EXIF_TYPES:
+#                    orientation = ANGLE[self._get_exif(img)]
+                # set animations
+                if self.slideshow_effect == "0":
+                    # add slide anim
+                    self._set_prop('Slide%d' % order[0], '0')
+                    self._set_prop('Slide%d' % order[1], '1')
+                else:
+                    # add random slide/zoom anim
+                    if self.slideshow_effect == "2":
+                        # add random slide/zoom anim
+                        self._anim(cur_img)
+                    # add fade anim, used for both fade and slide/zoom anim
+                    self._set_prop('Fade%d' % order[0], '0')
+                    self._set_prop('Fade%d' % order[1], '1')
+                # define next image
+                if cur_img == self.image1:
+                    cur_img = self.image2
+                    order = [2,1]
+                else:
+                    cur_img = self.image1
+                    order = [1,2]
+                # slideshow time in secs (we already slept for 1 second)
+                count = self.slideshow_time - 1
+                # display the image for the specified amount of time
+                while (not xbmc.abortRequested) and (not self.stop) and count > 0:
+                    count -= 1
+                    xbmc.sleep(1000)
+                # break out of the for loop if onScreensaverDeactivated is called
+                if  self.stop or xbmc.abortRequested:
+                    break
 
     def _get_items(self):
 	# image folder (fallback to video fanart if path is empty)
@@ -170,94 +196,6 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         random.shuffle(items, random.random)
         return items
 
-    def _start_show(self, items):
-        # start with image 1
-        cur_img = self.image1
-        order = [1,2]
-        # loop until onScreensaverDeactivated is called
-        while (not xbmc.abortRequested) and (not self.stop):
-            # iterate through all the images
-            for img in items:
-                # add image to gui
-                cur_img.setImage(img)
-                # give xbmc some time to load the image
-                if not self.startup:
-                    xbmc.sleep(1000)
-                self.startup = False
-                # get the file or foldername if enabled in settings
-                if self.slideshow_name != '0':
-                    if self.slideshow_name == '1':
-                        NAME, EXT = os.path.splitext(os.path.basename(img))
-                    elif self.slideshow_name == '2':
-                        ROOT, NAME = os.path.split(os.path.dirname(img))
-                    self.namelabel.setLabel(NAME)
-                # set animations
-                if self.slideshow_effect == "0":
-                    # add slide anim
-                    self._set_prop('Slide%d' % order[0], '0')
-                    self._set_prop('Slide%d' % order[1], '1')
-                else:
-                    # add random slide/zoom anim
-                    if self.slideshow_effect == "2":
-                        tex_width = 1280.0
-                        tex_height = 720.0
-                        xoff = 0
-                        yoff = 0
-# WIP
-                        # if the anim time is greater the 50 seconds, the image might be offscreen
-                        if __addon__.getSetting('scale') == 'false' and ((self.anim_time) > 50):
-                            # get image size
-                            size = self._getsize(img)
-                            if size:
-                                width  = float(size[0])
-                                height = float(size[1])
-                                # recalculate real image size to screensize
-                                aspect = width / height
-                                if aspect < (16.0/9.0):
-                                    # portrait
-                                    factor = height / 720.0
-                                    tex_width = width / factor
-                                    # calculate the amount of pix of the image on the screen
-                                    xinside = 1280.0 - (12.8 * float(self.anim_time))
-                                    # calculate the x offset
-                                    if (1280.0 - (2.0 * xinside)) > tex_width:
-                                       xoff = ((1280.0 - (2.0 * xinside)) - tex_width) / 2.0
-                                elif aspect > (16.0/9.0):
-                                    # landscape
-                                    factor = width / 1280.0
-                                    tex_height = height / factor
-                                    # calculate the amount of pix of the image on the screen
-                                    yinside = 720.0 - (7.2 * float(self.anim_time))
-                                    # calculate the y offset
-                                    if (720.0 - (2.0 * yinside)) > tex_height:
-                                       yoff = ((720.0 - (2.0 * yinside)) - tex_heigth) / 2.0
-# /WIP
-                        # add random slide/zoom anim
-                        self._anim(cur_img, xoff, yoff)
-                    # add fade anim, used for both fade and slide/zoom anim
-                    self._set_prop('Fade%d' % order[0], '0')
-                    self._set_prop('Fade%d' % order[1], '1')
-                # show the image
-                cur_img.setVisible(True)
-                # define next image
-                if cur_img == self.image1:
-                    cur_img = self.image2
-                    order = [2,1]
-                else:
-                    cur_img = self.image1
-                    order = [1,2]
-                # slideshow time in secs (we already slept for 1 second)
-                count = self.slideshow_time - 1
-                # display the image for the specified amount of time
-                while (not xbmc.abortRequested) and (not self.stop) and count > 0:
-                    count -= 1
-                    xbmc.sleep(1000)
-                # break out of the for loop if onScreensaverDeactivated is called
-                if  self.stop or xbmc.abortRequested:
-                    break
-                # set visible to false for the next image
-                cur_img.setVisible(False)
-
     def _walk(self, path):
         images = []
         folders = []
@@ -282,42 +220,77 @@ class Screensaver(xbmcgui.WindowXMLDialog):
                     images += self._walk(os.path.join(folder,item))
         return images
 
-    def _anim(self, cur_img, xoff, yoff):
+    def _get_exif(self, img):
+        # extract exif data from image file and find the orientation tag value
+        angle = '1'
+        try:
+            data = Image.open(img, 'r')
+            info = data._getexif()
+            if info:
+                for tag, value in info.items():
+                    key = TAGS.get(tag, tag)
+                    if key == 'Orientation':
+                        angle = str(value)
+        except:
+            pass
+        return angle
+
+    def _anim(self, cur_img):
         # reset position the current image
         cur_img.setPosition(0, 0)
         # pick a random anim
         number = random.randint(0,8)
         posx = 0
         posy = 0
-# WIP
-        adj_time = float(self.adj_time)
-        # calculate anim time:
-        if xoff != 0:
-            adj_time = int(((2560.0 / (2560.0 - (2.0 * xoff))) * adj_time) + 0.5)
-        elif yoff != 0:
-            adj_time = int(((1440.0 / (1440.0 - (2.0 * xoff))) * adj_time) + 0.5)
-# /WIP
-        # calculate posx and posy offset depending on the selected time per image
+        # add 1 sec fadeout time to showtime
+        anim_time = self.slideshow_time + 1
+        # set zoom level depending on the anim time
+        zoom = 110 + anim_time
         if number == 1 or number == 5 or number == 7:
-            posx = int(-1280 + (12.8 * self.anim_time) - xoff + 0.5)
+            posx = int(-1280 + (12.8 * anim_time) + 0.5)
         elif number == 2 or number == 6 or number == 8:
-            posx = int(1280 - (12.8 * self.anim_time) + xoff + 0.5)
+            posx = int(1280 - (12.8 * anim_time) + 0.5)
         if number == 3 or number == 5 or number == 6:
-            posy = int(-720 + (7.2 * self.anim_time) - yoff + 0.5)
+            posy = int(-720 + (7.2 * anim_time) + 0.5)
         elif number == 4 or number == 7 or number == 8:
-            posy = int(720 - (7.2 * self.anim_time) + yoff + 0.5)
+            posy = int(720 - (7.2 * anim_time) + 0.5)
         # position the current image
         cur_img.setPosition(posx, posy)
         # add the animation to the current image
-        cur_img.setAnimations(eval(EFFECTLIST[number] % (adj_time,adj_time)))
+        if number == 0:
+            cur_img.setAnimations(eval(EFFECTLIST[number] % (self.adj_time)))
+        else:
+            cur_img.setAnimations(eval(EFFECTLIST[number] % (self.adj_time, zoom, zoom, self.adj_time)))
 
-    def _getsize(self, image):
+    def _get_animspeed(self):
+        # find the skindir
+        if xbmcvfs.exists( __skinxbmc__ ):
+            # xbmc addon dir
+            skinxml = __skinxbmc__
+        elif xbmcvfs.exists( __skinhome__ ):
+            # user addon dir
+            skinxml = __skinhome__
+        else:
+            return
         try:
-            img = Image.open(image)
-            size = img.size
-            return size
+            # parse the skin addon.xml
+            self.xml = parse(skinxml)
+            # find all extension tags
+            tags = self.xml.documentElement.getElementsByTagName( 'extension' )
+            for tag in tags:
+                # find the effectslowdown attribute
+                for (name, value) in tag.attributes.items():
+                    if name == 'effectslowdown':
+                        anim = value
+                        return anim
         except:
             return
+
+    def _set_prop(self, name, value):
+        self.winid.setProperty('SlideView.%s' % name, value)
+
+    def _clear_prop(self, name):
+        self.winid.clearProperty('SlideView.%s' % name)
 
     def _exit(self):
         # exit when onScreensaverDeactivated gets called
@@ -330,17 +303,6 @@ class Screensaver(xbmcgui.WindowXMLDialog):
         self._clear_prop('Dim')
         self._clear_prop('Splash')
         self.close()
-
-# TODO exif rotation
-#    def get_exif(fn):
-#        from PIL.ExifTags import TAGS
-#        ret = {}
-#        i = Image.open(fn)
-#        info = i._getexif()
-#        for tag, value in info.items():
-#            decoded = TAGS.get(tag, tag)
-#            ret[decoded] = value
-#        return ret
 
 class MyMonitor(xbmc.Monitor):
     def __init__( self, *args, **kwargs ):
